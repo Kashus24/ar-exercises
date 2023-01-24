5f91ba9b521ff3e5c4cb69520e910d4c5da77d5c
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-23 23:37:40 -0500

    ex 7 done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter the store name"
+@name = gets.chomp
+
+new_store = Store.create(name: @name)
+puts new_store.errors.full_messages

784544355c1295e83180f4137dd850db1479f54c
Akashdeep Sekhon <kash@Akashdeeps-MacBook-Pro.local>
2023-01-23 23:21:03 -0500

    ex 6 done

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Kash", last_name: "Sekhon", hourly_rate: 90)
+@store2.employees.create(first_name: "Josh", last_name: "Drake", hourly_rate: 40)
+@store2.employees.create(first_name: "Nate", last_name: "Robs", hourly_rate: 105)
